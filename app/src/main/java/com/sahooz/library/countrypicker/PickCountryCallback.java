362f7200f4d02a4086d2573e18bc9c53b43ae75e
realstealthninja <realstealthninja@gmail.com>
2023-12-08 21:53:14 +0530

    fix: remove countrypicker and add it as a dependency instead

--- app/src/main/java/com/sahooz/library/countrypicker/PickCountryCallback.java
+++
@@ -1,6 +0,0 @@
-package com.sahooz.library.countrypicker;
-
-/* loaded from: classes.dex */
-public interface PickCountryCallback {
-    void onPick(Country country);
-}
--- app/src/main/java/com/sahooz/library/countrypicker/PyEntity.java
+++
@@ -1,6 +0,0 @@
-package com.sahooz.library.countrypicker;
-
-/* loaded from: classes.dex */
-public interface PyEntity {
-    String getPinyin();
-}
