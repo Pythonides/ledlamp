b726d3704d3fc77225597ea2e8e28d463735b789
realstealthninja <realstealthninja@gmail.com>
2023-12-08 21:51:06 +0530

    fix: remove githang and add it as a dependency instead

--- app/src/main/java/com/githang/statusbar/IStatusBar.java
+++
@@ -1,9 +0,0 @@
-package com.githang.statusbar;
-
-import android.view.Window;
-
-/* JADX INFO: Access modifiers changed from: package-private */
-/* loaded from: classes.dex */
-public interface IStatusBar {
-    void setStatusBarColor(Window window, int i);
-}
